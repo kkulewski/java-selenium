3e44bc7a33bd65e010b35627cc9ccf601ef1b4bb
Krzysztof Kulewski <krzysztofkulewski@gmail.com>
2018-05-27 13:58:19 +0200

    Rename + change packages

--- src/test/java/projekt3/GoogleSearchPageTest.java
+++ src/test/java/projekt3/GoogleSearchPageTest.java
@@ -1,4 +1,4 @@
-package projekt3.search;
+package projekt3;
 
 import static org.junit.jupiter.api.Assertions.*;
 
@@ -6,24 +6,26 @@ import org.junit.jupiter.api.AfterAll;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.support.PageFactory;
+import projekt3.pages.GoogleSearchPage;
 
-public class PageObjectFindByGoogleTest {
+public class GoogleSearchPageTest
+{
 
     private static WebDriver driver;
 
     @BeforeAll
     public static void setUpBeforeClass()
     {
-        System.setProperty("webdriver.gecko.driver", "resources/geckodriver");
-        driver = new FirefoxDriver();
+        System.setProperty("webdriver.chrome.driver", "resources/chromedriver.exe");
+        driver = new ChromeDriver();
     }
 
     @Test
     public void searchTest()
     {
-        PageFactoryGoogle page = PageFactory.initElements(driver, PageFactoryGoogle.class);
+        GoogleSearchPage page = PageFactory.initElements(driver, GoogleSearchPage.class);
         page.search("Hello world");
         assertTrue(page.titleContainsSearchPhrase());
     }
--- src/test/java/projekt3/pages/GoogleSearchPage.java
+++ src/test/java/projekt3/pages/GoogleSearchPage.java
@@ -1,4 +1,4 @@
-package projekt3.search;
+package projekt3.pages;
 
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
@@ -7,7 +7,8 @@ import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
-public class PageFactoryGoogle {
+public class GoogleSearchPage
+{
 
     private String searchPhrase;
 
@@ -16,21 +17,21 @@ public class PageFactoryGoogle {
     private WebDriver driver;
     private WebDriverWait wait;
   
-    public PageFactoryGoogle(WebDriver driver)
+    public GoogleSearchPage(WebDriver driver)
     {
         this.driver = driver;
         driver.get("http://www.google.com/");
         wait = new WebDriverWait(driver,30);
     }
   
-    void search(String text)
+    public void search(String text)
     {
         searchPhrase = text;
         q.sendKeys(text);
         q.submit();
     }
   
-    boolean titleContainsSearchPhrase()
+    public boolean titleContainsSearchPhrase()
     {
         q = wait.until(ExpectedConditions.visibilityOfElementLocated(By.className("q")));
         Boolean result = driver.getTitle().contains(searchPhrase);
