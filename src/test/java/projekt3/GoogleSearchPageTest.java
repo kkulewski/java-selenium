b6252c1560d244586692b5f7e2b8079bfc238da8
Krzysztof Kulewski <krzysztofkulewski@gmail.com>
2018-05-27 17:38:58 +0200

    Add firefox tests

--- src/test/java/projekt3/GoogleSearchPageTest.java
+++ src/test/java/projekt3/GoogleSearchPageTest.java
@@ -2,44 +2,60 @@ package projekt3;
 
 import static org.junit.jupiter.api.Assertions.*;
 
-import org.junit.jupiter.api.AfterAll;
-import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.Test;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
 import org.openqa.selenium.support.PageFactory;
 import projekt3.pages.GoogleSearchPage;
 
 public class GoogleSearchPageTest
 {
-
     private static WebDriver driver;
 
-    @BeforeAll
-    public static void setUpBeforeClass()
+    @Test
+    public void searchSuccessfulChromeTest()
     {
         BrowserHandler.setBrowser(Browser.Chrome);
         driver = new ChromeDriver();
-    }
-
-    @Test
-    public void searchSuccessfulTest()
-    {
         GoogleSearchPage page = PageFactory.initElements(driver, GoogleSearchPage.class);
         page.search("Hello world");
         assertTrue(page.titleContainsSearchPhrase());
     }
 
     @Test
-    public void searchFailedTest()
+    public void searchFailedChromeTest()
     {
+        BrowserHandler.setBrowser(Browser.Chrome);
+        driver = new ChromeDriver();
         GoogleSearchPage page = PageFactory.initElements(driver, GoogleSearchPage.class);
         page.search("asdfghjkasdfghmnnvxxzqwertyuasdfgh");
         assertTrue(page.titleContainsSearchPhrase());
     }
 
-    @AfterAll
-    public static void tearDownAfterClass()
+    @Test
+    public void searchSuccessfulFirefoxTest()
+    {
+        BrowserHandler.setBrowser(Browser.Firefox);
+        driver = new FirefoxDriver();
+        GoogleSearchPage page = PageFactory.initElements(driver, GoogleSearchPage.class);
+        page.search("Hello world");
+        assertTrue(page.titleContainsSearchPhrase());
+    }
+
+    @Test
+    public void searchFailedFirefoxTest()
+    {
+        BrowserHandler.setBrowser(Browser.Firefox);
+        driver = new FirefoxDriver();
+        GoogleSearchPage page = PageFactory.initElements(driver, GoogleSearchPage.class);
+        page.search("asdfghjkasdfghmnnvxxzqwertyuasdfgh");
+        assertTrue(page.titleContainsSearchPhrase());
+    }
+
+    @AfterEach
+    public void tearDownAfterClass()
     {
         driver.quit();
     }
