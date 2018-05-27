572728b68d1453f2976c4c121ff7a0df4dcf91fb
Krzysztof Kulewski <krzysztofkulewski@gmail.com>
2018-05-27 03:30:32 +0200

    Add maven project

---
+++ src/test/java/projekt3/AppTest.java
@@ -0,0 +1,19 @@
+package projekt3;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+public class AppTest
+{
+    @BeforeEach
+    void setup()
+    {
+
+    }
+
+    @Test
+    void mainTest()
+    {
+
+    }
+}
